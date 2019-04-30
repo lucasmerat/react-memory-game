e3fa886aca183fe895c12ded3090adb2b03bbac0
Lucas Merat <lucas.merat@gmail.com>
2019-04-30 10:42:22 -0500

    Mobile Responsiveness

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,9 @@ function Header({score, topScore, message}) {
     <div>
       <nav>
         <div className="nav-box row black">
-            <div className="col s4 left-align"><a href="/"><img className="logo responsive-img" src="/assets/img/logo.png" alt=""/></a></div>
-            <div className="header-box-center col s4 center-align">{message}</div>
-            <div className="header-box-right col s4 right-align"><span>Guessed correctly: {score} | High Score: {topScore}</span></div>
+            <div className="header-box-left col s12 l4 left-align"><a href="/"><img className="logo responsive-img" src="/assets/img/logo.png" alt=""/></a></div>
+            <div className="header-box-center col s12 l4 center-align">{message}</div>
+            <div className="header-box-right col s12 l4 right-align"><span>Guessed correctly: {score} | High Score: {topScore}</span></div>
         </div>
       </nav>
     </div>
